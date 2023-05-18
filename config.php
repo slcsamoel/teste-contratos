ba484972254a75dcc84af0900f8b0c0fcf283d69
samoel <samuellopescosta@gmail.com>
2023-05-18 13:55:49 -0300

    ajsute de config.php

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 
  //Mysql 
- const DBDRIVE =  'mysql';
- const DBHOST  =  '192.168.1.137';
- const DBPORT  =  '3333';
+const DBDRIVE =  'mysql';
+ const DBHOST  =  '127.0.0.1';
+ const DBPORT  =  '3306';
  const DBNAME  =  'teste_contratos';
  const DBUSER  =  'root';
- const DBPASS  =  'root87&*!QW87';
+ const DBPASS  =  '';
  
 //server 
 const APP_URL = 'http://contratos.my/';
@@ -14,12 +14,3 @@ const APP_URL = 'http://contratos.my/';
  
 
 
-// const DBDRIVE =  'mysql';
-//  const DBHOST  =  '127.0.0.1';
-//  const DBPORT  =  '3306';
-//  const DBNAME  =  'teste_contratos';
-//  const DBUSER  =  'root';
-//  const DBPASS  =  '';
- 
-// //server 
-// const APP_URL = 'http://contratos.my/';
